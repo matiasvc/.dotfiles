e0d46b56f353c3ebbc593814bd66e6e393420116
Matias Christensen <mattivc@gmail.com>
2021-09-25 21:29:17 +0200

    Remove unused files

--- zsh/alias.zsh
+++
@@ -1,6 +0,0 @@
-alias zshconfig="${EDITOR} ${HOME}/.zshrc"
-alias ohmyzshconfig="${EDITOR} ${HOME}/.oh-my-zsh"
-alias i3config="${EDITOR} ${HOME}/.dotfiles/i3/config"
-alias vimconfig="${EDITOR} ${HOME}/.vimrc"
-alias kittyconfig="${EDITOR} ${HOME}/.dotfiles/kitty/kitty.conf"
-
