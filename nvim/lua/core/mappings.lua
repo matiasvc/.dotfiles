c77d4d9196c4b9a2c044604d29c800917dd3f138
Matias Christensen <mattivc@gmail.com>
2021-11-05 13:25:36 +0100

    Fri Nov  5 13:25:36 CET 2021

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -4,26 +4,6 @@ local map = require('core.functions').map
 map('n', '<Space>', '<NOP>')
 vim.g.mapleader = ' '
 
--- Window navigation
-map('n', '<M-j>', '<C-w>j')
-map('n', '<M-k>', '<C-w>k')
-map('n', '<M-h>', '<C-w>h')
-map('n', '<M-l>', '<C-w>l')
-
--- Window resizing
-map('n', '<C-j>', ':resize -2<CR>')
-map('n', '<C-k>', ':resize +2<CR>')
-map('n', '<C-h>', ':vertical resize -2<CR>')
-map('n', '<C-l>', ':vertical resize +2<CR>')
-
--- Window splitting
-map('n', '<leader>ws', ':split<CR>')
-map('n', '<leader>wv', ':vertical split<CR>')
-map('n', '<leader>wt', ':tab split<CR>')
-
--- Window rotating
-map('n', '<leader>wr', '<C-w><C-r>')
-
 -- Telescope finder
 map('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<CR>]])
 map('n', '<leader>fe', [[<cmd>lua require('telescope.builtin').file_browser({hidden=true})<CR>]])
