ec5423862375c3b11daddc4b30d7eb50f6b51daa
Matias Varnum <mattivc@gmail.com>
2024-08-06 17:49:37 +0200

    Tue Aug  6 17:49:37 CEST 2024

--- setup.sh
+++ setup.sh
@@ -3,71 +3,75 @@
 # This script symlinks all the config files to the right location.
 
 # Path of folder where this script is, which should be the .dotfiles folder.
-DOTFILES=$(dirname $(realpath -s $0))
+DOTFILES=$(dirname $(realpath -s "$0"))
 
 # .profile setup
-rm -f ${HOME}/.profile
-ln -s ${DOTFILES}/profile/.profile ${HOME}/.profile
+rm -f "${HOME}"/.profile
+ln -s "${DOTFILES}"/profile/.profile "${HOME}"/.profile
 
 # alacritty setup
-rm -f ${HOME}/.config/alacritty/alacritty.yml
-mkdir -p ${HOME}/.config/alacritty
-ln -s ${DOTFILES}/alacritty/alacritty.yml ${HOME}/.config/alacritty/alacritty.yml
+rm -f "${HOME}"/.config/alacritty/alacritty.yml
+mkdir -p "${HOME}"/.config/alacritty
+ln -s "${DOTFILES}"/alacritty/alacritty.yml "${HOME}"/.config/alacritty/alacritty.yml
 
 # zsh setup
-rm -f ${HOME}/.zshrc
-ln -s ${DOTFILES}/zsh/.zshrc ${HOME}/.zshrc
+rm -f "${HOME}"/.zshrc
+ln -s "${DOTFILES}"/zsh/.zshrc "${HOME}"/.zshrc
 
-mkdir -p ${HOME}/.zsh/
-rm -f ${HOME}/.zsh/antigen.zsh
-ln -s ${DOTFILES}/zsh/antigen.zsh ${HOME}/.zsh/antigen.zsh
-rm -f ${HOME}/.zsh/p10k.zsh
-ln -s ${DOTFILES}/zsh/p10k.zsh ${HOME}/.zsh/p10k.zsh
+mkdir -p "${HOME}"/.zsh/
+rm -f "${HOME}"/.zsh/antigen.zsh
+ln -s "${DOTFILES}"/zsh/antigen.zsh "${HOME}"/.zsh/antigen.zsh
+rm -f "${HOME}"/.zsh/p10k.zsh
+ln -s "${DOTFILES}"/zsh/p10k.zsh "${HOME}"/.zsh/p10k.zsh
 
-rm -rf ${HOME}/.zsh/external
-ln -s ${DOTFILES}/zsh/external ${HOME}/.zsh/external
+rm -rf "${HOME}"/.zsh/external
+ln -s "${DOTFILES}"/zsh/external "${HOME}"/.zsh/external
 
 # nvim setup
-rm -rf ${HOME}/.config/nvim
-ln -s ${DOTFILES}/nvim ${HOME}/.config/nvim
+rm -rf "${HOME}"/.config/nvim
+ln -s "${DOTFILES}"/nvim "${HOME}"/.config/nvim
 
 # git setup
-rm -f ${HOME}/.gitconfig
-ln -s ${DOTFILES}/git/.gitconfig ${HOME}/.gitconfig
+rm -f "${HOME}"/.gitconfig
+ln -s "${DOTFILES}"/git/.gitconfig "${HOME}"/.gitconfig
 
 # i3 setup
-rm -rf ${HOME}/.config/i3
-mkdir -p ${HOME}/.config
-ln -s ${DOTFILES}/i3 ${HOME}/.config/i3
+rm -rf "${HOME}"/.config/i3
+mkdir -p "${HOME}"/.config
+ln -s "${DOTFILES}"/i3 "${HOME}"/.config/i3
 
 # i3blocks setup
-rm -rf ${HOME}/.config/i3blocks
-ln -s ${DOTFILES}/i3blocks ${HOME}/.config/i3blocks
+rm -rf "${HOME}"/.config/i3blocks
+ln -s "${DOTFILES}"/i3blocks "${HOME}"/.config/i3blocks
 
 # ranger setup
-rm -rf ${HOME}/.config/ranger
-ln -s ${DOTFILES}/ranger ${HOME}/.config/ranger
+rm -rf "${HOME}"/.config/ranger
+ln -s "${DOTFILES}"/ranger "${HOME}"/.config/ranger
 
 # ssh config setup
-mkdir -p ${HOME}/.ssh
-rm -f ${HOME}/.ssh/config
-ln -s ${DOTFILES}/ssh/config ${HOME}/.ssh/config
+mkdir -p "${HOME}"/.ssh
+rm -f "${HOME}"/.ssh/config
+ln -s "${DOTFILES}"/ssh/config "${HOME}"/.ssh/config
 
 # jupyter setup
-mkdir -p ${HOME}/.jupyter
-rm -f ${HOME}/.jupyter/jupyter_notebook_config.py
-ln -s ${DOTFILES}/jupyter/jupyter_notebook_config.py ${HOME}/.jupyter/
+mkdir -p "${HOME}"/.jupyter
+rm -f "${HOME}"/.jupyter/jupyter_notebook_config.py
+ln -s "${DOTFILES}"/jupyter/jupyter_notebook_config.py "${HOME}"/.jupyter/
 
 # redshift setup
-rm -f ${HOME}/.config/redshift.conf
-ln -s ${DOTFILES}/redshift/redshift.conf ${HOME}/.config/redshift.conf
+rm -f "${HOME}"/.config/redshift.conf
+ln -s "${DOTFILES}"/redshift/redshift.conf "${HOME}"/.config/redshift.conf
 
 # udev rules
 sudo rm -f /etc/udev/rules.d/90-brightnessctl.rules
-sudo ln -s ${DOTFILES}/udev/90-brightnessctl.rules /etc/udev/rules.d/
+sudo ln -s "${DOTFILES}"/udev/90-brightnessctl.rules /etc/udev/rules.d/
 
 # gdb setup
-rm -f ${HOME}/.gdbinit
-ln -s ${DOTFILES}/gdb/.gdbinit ${HOME}/.gdbinit
+rm -f "${HOME}"/.gdbinit
+ln -s "${DOTFILES}"/gdb/.gdbinit "${HOME}"/.gdbinit
+
+# cuda-gdb setup
+rm -f "${HOME}"/.cuda-gdbinit
+ln -s "${DOTFILES}"/cuda-gdb/.cuda-gdbinit "${HOME}"/.cuda-gdbinit
 
 
